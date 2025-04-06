cf66f6169936285cdbf7e59aa30fc6cfa3c5d128
leylicowell <leyli.cowell@gmail.com>
2025-04-06 18:19:53 +0300

    Add reformat_txt function to correct analysis file

--- analyses/00_wrangle-hurricane-data.R
+++ analyses/00_wrangle-hurricane-data.R
@@ -29,7 +29,63 @@ pacific_data <- read_lines(here("data", "raw", "pacific-data.txt"))
 #------------------------------------------------------------------------------
 # we focus on the relevant columns for our analysis to simplify our data frames
 # we also reformat our .txt data into a data frame object
+# the function below is only used in this .R file
 #------------------------------------------------------------------------------
+
+reformat_txt <- function(data){
+  
+  STORM_ID <- c()
+  NAME <- c()
+  DATE <- c()
+  TIME <- c()
+  RECORD_ID <- c()
+  STATUS <- c()
+  LAT <- c()
+  LON <- c()
+  WIND <- c()
+  PRESSURE <- c()
+  
+  i <- 1
+  while(i < length(data)){
+    header_line <- str_split_1(data[i], pattern = ",")
+    header_line <- gsub(" ", "", header_line) # remove whitespace
+    storm_id <- header_line[1]
+    name <- header_line[2]
+    n_track <- header_line[3] # number of track entries
+    
+    for (j in 1:n_track){
+      i = i + 1
+      data_line <- str_split_1(data[i], pattern = ",")
+      data_line <- gsub(" ", "", data_line) # remove whitespace
+      STORM_ID <- c(STORM_ID, storm_id)
+      NAME <- c(NAME, name)
+      DATE <- c(DATE, data_line[1])
+      TIME <- c(TIME,data_line[2])
+      RECORD_ID <- c(RECORD_ID, data_line[3])
+      STATUS <- c(STATUS, data_line[4])
+      LAT <- c(LAT, data_line[5])
+      LON <- c(LON, data_line[6])
+      WIND <- c(WIND, data_line[7])
+      PRESSURE <- c(PRESSURE, data_line[8])
+    }
+    i = i + 1
+  }
+  
+  new_data <- data.frame(
+    STORM_ID = STORM_ID,
+    NAME = NAME,
+    DATE = DATE,
+    TIME = TIME,
+    RECORD_ID = RECORD_ID,
+    STATUS = STATUS,
+    LAT = LAT,
+    LON = LON,
+    WIND = WIND,
+    PRESSURE = PRESSURE
+  )
+  
+  return(new_data)
+}
 
 atlantic_data <- reformat_txt(atlantic_data)
 pacific_data <- reformat_txt(pacific_data)
