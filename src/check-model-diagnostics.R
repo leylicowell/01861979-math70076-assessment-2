e477a9f89d6b2844cca15f1c106f861ad3760720
leylicowell <leyli.cowell@gmail.com>
2025-04-30 17:21:54 +0100

    Create function to check model convergence

---
+++ src/check-model-diagnostics.R
@@ -0,0 +1,82 @@
+#' Check model convergence diagnostics for Stan models
+#'
+#' @param model CmdStanMCMC fitted Stan model object
+#' @param var variables/parameters from our Stan model
+#' @param warmup TRUE if warmup was included in stan model or FALSE otherwise
+#' @param warmup_nbr number of iterations in model which were part of the warmup
+#'
+#' @returns table of model convergence diagnostics for 5 worst chosen parameters
+#' 
+#' @import cmdstanr
+#' @importFrom data.table as.data.table
+#' @importFrom knitr kbl
+#' @importFrom kableExtra kable_styling
+#' @import ggplot2
+#' @export 
+#'
+#' @examples 
+#' fit <- cmdstanr_example("logistic")
+#' check_model_diagnostics(fit, c("alpha", "beta"), warmup = FALSE, warmup_nbr = 0)
+#' 
+check_model_diagnostics <- function(model_fit, var, warmup = TRUE, warmup_nbr = 500){
+  # summarise model parameter measures for diagnostics table
+  model_summary <- model_fit$summary(
+    variables = var,
+    posterior::default_summary_measures(),
+    posterior::default_convergence_measures())
+  
+  # extract samples for trace plot and pairwise plot
+  model_draws <- model_fit$draws(
+    variables = var,
+    inc_warmup = warmup, format = "draws_array")
+  
+  # sort by smallest ess_bulk
+  model_summary  <- as.data.table(model_summary )
+  model_summary  <- model_summary[order(ess_bulk),]
+  
+  # print table of parameter convergence diagnostics
+  print(kbl(model_summary, caption = 'Model diagnostics', longtable = TRUE) %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "condensed"), 
+                  font_size = 12))
+  
+  # save table of 5 (or all parameters if there are less than 5) worst performing parameters
+  t1 <- kbl(subset(model_summary[1:min(5, nrow(model_summary)),], 
+                   select = c(variable, rhat, ess_bulk, ess_tail)),
+            caption = 'Model diagnostics for 5 worst performing parameters', 
+            longtable = TRUE) %>%
+    kable_styling(bootstrap_options = c("striped", "hover", "condensed"), 
+                  font_size = 12) 
+  
+  
+  # make trace plot of worst performing parameter
+  model1_worst_var <- model_summary$variable[ which.min(model_summary$ess_bulk)]
+  
+  print(bayesplot:::mcmc_trace(model_draws,  
+                         pars = model1_worst_var, 
+                         n_warmup = warmup_nbr, 
+                         facet_args = list(nrow = 1))+ 
+    theme_bw())
+  
+  # only print pairwise plot if we have more than one parameter
+  if(nrow(model_summary) > 1){
+    # make pairwise plot of 4 (or all, if less than 4) model parameters
+    worst_param <- model_summary[1:min(4, nrow(model_summary)),]$variable
+    model_draws <- model_fit$draws(
+      variables = worst_param,inc_warmup = FALSE,
+      format = "draws_array")
+    
+    bayesplot::color_scheme_set('viridisC')
+    print(bayesplot::mcmc_pairs(model_draws, 
+                          pars = worst_param, 
+                          diag_fun = "dens", 
+                          off_diag_fun = "hex"))
+  }
+    
+  # return diagnostics table for report
+  return(t1)
+}
+
+
+
+
+
---
+++ tests/testthat/test-check-model-diagnostics.R
@@ -0,0 +1,20 @@
+# check_model_diagnostics example
+# should print trace plot without warmup and no pairs plot
+cmdstanr::cmdstanr_example("logistic")
+testthat::expect_s3_class(check_model_diagnostics(fit, 
+                                                  "alpha", 
+                                                  warmup = FALSE, 
+                                                  warmup_nbr = 0), 
+                          "knitr_kable")
+
+# check_model_diagnostics with invalid parameters
+fit <- cmdstanr::cmdstanr_example("logistic")
+testthat::expect_error(check_model_diagnostics(fit, "not_a_real_param"))
+
+
+# check_model_diagnostics with warmup and more than one parameter
+fit <- cmdstanr::cmdstanr_example("logistic", save_warmup = TRUE, iter_warmup = 500)
+testthat::expect_s3_class(check_model_diagnostics(fit, c("alpha", "beta")),
+                                 "knitr_kable")
+
+
